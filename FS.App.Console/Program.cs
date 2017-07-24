3682865905844c7a2ce11dad0668569ca90a7f33
Sawson Moghadaszadeh <sawsonh@yahoo.com>
2017-07-24 11:32:57 -0500

    Imciplit typing

--- FS.App.Console/Program.cs
+++ FS.App.Console/Program.cs
@@ -10,7 +10,7 @@ namespace FS.App.Console
     {
         static void Main(string[] args)
         {
-            DateTime dateTime = DateTime.Now.AddHours(-6);
+            var dateTime = DateTime.Now.AddHours(-6);
             if (args.Any())
                 if (!DateTime.TryParse(args[0], out dateTime))
                     dateTime = DateTime.Now.AddHours(-6);
